f474ae6ab6ed61d5825ed76fda908e0ae3174e12
Nguyen <nguyen@nguyens-MacBook-Pro.local>
2021-06-10 23:12:31 +0700

    update css

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 window.onscroll = () => {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("navbar").style.background = "black";
-    document.getElementById("navbar").style.padding = "10px 0";
+    document.getElementById("navbar").style.background = "#111";
+    document.getElementById("navbar").style.padding = "0.5rem 0";
 
     } else {
         document.getElementById("navbar").style.background = "transparent";
-        document.getElementById("navbar").style.padding = "30px 0";
+        document.getElementById("navbar").style.padding = "1.5rem 0";
 
     }
 }
